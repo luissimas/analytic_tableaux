4004671c9a353b53ef9a125b69ceb39e24a78faf
Luis Simas <luissimas@protonmail.com>
2021-07-26 14:22:28 -0300

    Adding support for sequent parsing

--- lib/parser.ex
+++ lib/parser.ex
@@ -5,10 +5,32 @@ defmodule Parser do
 
   @spec parse(binary()) :: map()
   def parse(input) do
+    [antecedents, consequent] = String.split(input, "|-")
+
+    antecedents =
+      antecedents
+      |> String.split(",")
+      |> Enum.map(fn formula ->
+        formula
+        |> parse_formula()
+        |> add_sign(:T)
+      end)
+
+    consequent =
+      consequent
+      |> parse_formula()
+      |> add_sign(:F)
+
+    antecedents ++ [consequent]
+  end
+
+  defp parse_formula(input) do
     {:ok, tokens, _} = input |> String.to_charlist() |> :lexer.string()
 
     {:ok, result} = :parser.parse(tokens)
 
     result
   end
+
+  defp add_sign(formula, sign), do: %{sign: sign, formula: formula}
 end
