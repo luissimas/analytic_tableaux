1d068a8f57dd54b0babe72cfe0131be7e840145d
Luis Simas <luissimas@protonmail.com>
2021-08-12 17:31:09 -0300

    Sorting formulas in the stack

--- lib/rules.ex
+++ lib/rules.ex
@@ -3,6 +3,7 @@ defmodule Rules do
   Tableaux expansion rules
   """
 
+  @spec apply_rule(TreeNode.t()) :: %{tree: BinTree.t(), expansion: [TreeNode.t()]}
   def apply_rule(formula) do
     type = get_type(formula)
     expansion = get_expansion(formula)
@@ -28,12 +29,29 @@ defmodule Rules do
     end
   end
 
+  @spec can_expand?(TreeNode.t()) :: boolean()
   def can_expand?(formula) do
     case get_type(formula) do
       :atom -> false
       _ -> true
     end
   end
+
+  @spec compare_operators(atom(), atom()) :: boolean()
+  def compare_operators(:linear, _), do: true
+  def compare_operators(_, :branch), do: true
+  def compare_operators(_, _), do: false
+
+  @spec get_type(TreeNode.t()) :: :linear | :branch | :atom
+  def get_type(%{sign: :T, formula: {:and, _, _}}), do: :linear
+  def get_type(%{sign: :F, formula: {:and, _, _}}), do: :branch
+  def get_type(%{sign: :T, formula: {:or, _, _}}), do: :branch
+  def get_type(%{sign: :F, formula: {:or, _, _}}), do: :linear
+  def get_type(%{sign: :T, formula: {:implies, _, _}}), do: :branch
+  def get_type(%{sign: :F, formula: {:implies, _, _}}), do: :linear
+  def get_type(%{sign: :T, formula: {:not, _}}), do: :linear
+  def get_type(%{sign: :F, formula: {:not, _}}), do: :linear
+  def get_type(%{formula: _}), do: :atom
 
   # Tp&q => Tp, Tq
   defp get_expansion(%{sign: :T, formula: {:and, a, b}}) do
@@ -92,14 +110,4 @@ defmodule Rules do
 
   # F/T p => F/T p
   defp get_expansion(%{formula: _} = formula), do: formula
-
-  defp get_type(%{sign: :T, formula: {:and, _, _}}), do: :linear
-  defp get_type(%{sign: :F, formula: {:and, _, _}}), do: :branch
-  defp get_type(%{sign: :T, formula: {:or, _, _}}), do: :branch
-  defp get_type(%{sign: :F, formula: {:or, _, _}}), do: :linear
-  defp get_type(%{sign: :T, formula: {:implies, _, _}}), do: :branch
-  defp get_type(%{sign: :F, formula: {:implies, _, _}}), do: :linear
-  defp get_type(%{sign: :T, formula: {:not, _}}), do: :linear
-  defp get_type(%{sign: :F, formula: {:not, _}}), do: :linear
-  defp get_type(%{formula: _}), do: :atom
 end
--- lib/tableaux.ex
+++ lib/tableaux.ex
@@ -26,10 +26,6 @@ defmodule Tableaux do
   def is_valid?(nil, _, _), do: true
 
   def is_valid?(%{left: nil, right: nil, value: value} = tree, path, to_expand) do
-    # IO.inspect(tree)
-    # IO.inspect(to_expand)
-    # IO.puts("\n")
-
     cond do
       closed?(value, path) ->
         true
@@ -39,9 +35,9 @@ defmodule Tableaux do
         # then call is_valid? on the new tree created from the expansions
         %{tree: expanded_tree, expansion: expanded} = Rules.apply_rule(value)
 
-        new_to_expand = Enum.filter(expanded, &Rules.can_expand?/1)
+        new_to_expand = sort_formulas(Enum.filter(expanded, &Rules.can_expand?/1) ++ to_expand)
 
-        is_valid?(BinTree.add(tree, expanded_tree), path, new_to_expand ++ to_expand)
+        is_valid?(BinTree.add(tree, expanded_tree), path, new_to_expand)
 
       Enum.empty?(to_expand) ->
         false
@@ -53,7 +49,7 @@ defmodule Tableaux do
 
         %{tree: expanded_tree, expansion: expanded} = Rules.apply_rule(head)
 
-        new_to_expand = Enum.filter(expanded, &Rules.can_expand?/1) ++ tail
+        new_to_expand = sort_formulas(Enum.filter(expanded, &Rules.can_expand?/1) ++ tail)
 
         is_valid?(BinTree.add(tree, expanded_tree), path, new_to_expand)
     end
@@ -71,7 +67,15 @@ defmodule Tableaux do
   def closed?(formula, path), do: Enum.any?(path, &contradiction?(formula, &1))
 
   @spec contradiction?(TreeNode.t(), TreeNode.t()) :: boolean()
-  def contradiction?(%{sign: :F, formula: formula}, %{sign: :T, formula: formula}), do: true
-  def contradiction?(%{sign: :T, formula: formula}, %{sign: :F, formula: formula}), do: true
+  def contradiction?(%{sign: s1, formula: f}, %{sign: s2, formula: f}) when s1 != s2, do: true
   def contradiction?(_, _), do: false
+
+  @spec sort_formulas([TreeNode.t()]) :: [TreeNode.t()]
+  defp sort_formulas(list) do
+    Enum.sort_by(
+      list,
+      &Rules.get_type/1,
+      &Rules.compare_operators(&1, &2)
+    )
+  end
 end
