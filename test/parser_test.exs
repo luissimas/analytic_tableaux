a5898ac077910c88d5011c7907a21691fb76f4e8
Luís Simas <luissimas@protonmail.com>
2023-11-17 09:28:55 -0300

    test: fix parser tests

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -2,20 +2,22 @@ defmodule ParserTest do
   use ExUnit.Case, async: true
 
   test "parses a simple sequent" do
-    assert Parser.parse("p->q,p|-q") == [
-             %Formula{
-               formula: {:implies, :p, :q},
-               sign: :T
-             },
-             %Formula{
-               formula: :p,
-               sign: :T
-             },
-             %Formula{
-               formula: :q,
-               sign: :F
-             }
-           ]
+    assert Parser.parse("p->q,p|-q") ==
+             {:ok,
+              [
+                %Formula{
+                  formula: {:implies, :p, :q},
+                  sign: :T
+                },
+                %Formula{
+                  formula: :p,
+                  sign: :T
+                },
+                %Formula{
+                  formula: :q,
+                  sign: :F
+                }
+              ]}
   end
 
   test "ignores spaces on input" do
@@ -27,27 +29,29 @@ defmodule ParserTest do
   end
 
   test "parses complex sequents" do
-    assert Parser.parse("p&!q->r|t, t->q|!r, r&!q, p|t |- p->!r") == [
-             %Formula{
-               formula: {:implies, {:and, :p, {:not, :q}}, {:or, :r, :t}},
-               sign: :T
-             },
-             %Formula{
-               formula: {:implies, :t, {:or, :q, {:not, :r}}},
-               sign: :T
-             },
-             %Formula{
-               formula: {:and, :r, {:not, :q}},
-               sign: :T
-             },
-             %Formula{
-               formula: {:or, :p, :t},
-               sign: :T
-             },
-             %Formula{
-               formula: {:implies, :p, {:not, :r}},
-               sign: :F
-             }
-           ]
+    assert Parser.parse("p&!q->r|t, t->q|!r, r&!q, p|t |- p->!r") ==
+             {:ok,
+              [
+                %Formula{
+                  formula: {:implies, {:and, :p, {:not, :q}}, {:or, :r, :t}},
+                  sign: :T
+                },
+                %Formula{
+                  formula: {:implies, :t, {:or, :q, {:not, :r}}},
+                  sign: :T
+                },
+                %Formula{
+                  formula: {:and, :r, {:not, :q}},
+                  sign: :T
+                },
+                %Formula{
+                  formula: {:or, :p, :t},
+                  sign: :T
+                },
+                %Formula{
+                  formula: {:implies, :p, {:not, :r}},
+                  sign: :F
+                }
+              ]}
   end
 end
